d6fa9c5a5e0ecd7fd8bf957db7f40ec7c4bb814a
semrithiro <rithiro@gmail.com>
2018-12-01 07:42:24 +0700

    init xcode project

---
+++ array_dictionary.playground/Contents.swift
@@ -0,0 +1,10 @@
+var shoppingList = ["catfish", "water", "tulips"]
+
+shoppingList[1] = "bottle of water"
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic"
+]
+
+print(occupations["Hi"] ?? "404 Not found")
